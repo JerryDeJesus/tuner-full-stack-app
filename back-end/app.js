6b4210a8f89b78a504996efcfb5a5e103a7c71d0
Jerry De Jesus <jdejesus7@live.com>
2022-02-08 17:17:35 -0500

    merged backend portion

---
+++ back-end/app.js
@@ -0,0 +1,19 @@
+const cors = require("cors");
+const express = require("express");
+const songController = require("./controllers/songController.js");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Tuner App");
+});
+
+app.use("/songs", songController);
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+module.exports = app;
